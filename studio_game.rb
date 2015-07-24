19bd851bcffa7541e897df8c1df0b2eb47b6d7da
kangkyu <kangkyu1111@gmail.com>
2015-07-24 15:21:43 -0700

    make the game more interactive

--- studio_game.rb
+++ studio_game.rb
@@ -9,3 +9,18 @@ knuckleheads = Game.new("Knuckleheads")
 knuckleheads.add_player(player3)
 knuckleheads.add_player(player2)
 knuckleheads.add_player(player1)
+
+loop do
+  puts "\nHow many game rounds? ('quit' to exit)"
+  answer = gets.chomp.downcase
+
+  case answer
+  when /^\d+$/
+    knuckleheads.play(answer.to_i)
+  when 'exit', 'quit'
+    knuckleheads.print_stats
+    break
+  else
+    puts "Please enter a number or 'quit'"
+  end
+end
